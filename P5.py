ac2c0ddc5f3c7ede6381ed31d99398f4e4dbb8c4
Sergey <vip.gastat@mail.ru>
2024-11-19 08:29:21 +0300

    Добавлен 6 пункт и внесены доработки в 5 пункт

--- P5.py
+++ P5.py
@@ -8,14 +8,12 @@ import math
 #----------------------------------------------------------------------------------------------------------------------
 # ACF of video signal
 
-tau = np.arange(-100, 100)
+tau = np.arange(-100, 100, 0.01)
 R = np.zeros(len(tau))
 
 for i in range(len(tau)):
-  if tau[i] >= -90 and tau[i] <= 0:
-    R[i] = (math.pow(tau[i], 3) / -48600) + 2.5 * tau[i] + 210
-  elif tau[i] >= 0 and tau[i] <= 90:
-    R[i] = (math.pow(tau[i], 3) / 48600) - 2.5 * tau[i] + 210
+  if tau[i] >= -90 and tau[i] <= 90:
+    R[i] = (math.pow(abs(tau[i]), 3) / 48600) - 2.5 * abs(tau[i]) + 210
 
 plt.plot(tau, R)
 plt.grid()
@@ -30,10 +28,8 @@ tau = np.arange(-100, 100)
 R = np.zeros(len(tau))
 
 for i in range(len(tau)):
-  if tau[i] >= -90 and tau[i] <= 0:
-    R[i] = ((math.pow(tau[i], 3) / -48600) + 2.5 * tau[i] + 210) * math.cos(7 * tau[i]) * (1 / 2)
-  elif tau[i] >= 0 and tau[i] <= 90:
-    R[i] = ((math.pow(tau[i], 3) / 48600) - 2.5 * tau[i] + 210) * math.cos(7 * tau[i]) * (1 / 2)
+  if tau[i] >= -90 and tau[i] <= 90:
+    R[i] = ((math.pow(abs(tau[i]), 3) / 48600) - 2.5 * abs(tau[i]) + 210) * math.cos(7 * abs(tau[i])) * (1 / 2)
 
 plt.plot(tau, R)
 plt.grid()
